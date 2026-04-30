741c350240201b11f261f5392f62700dab9f5e4c
jprivet-dev <jprivet.dev@gmail.com>
2026-04-30 16:52:41 +0200

    feat(PdfDebugComparisonResult.php): remove actual, reference & diff pathes

--- src/ValueObject/PdfDebugComparisonResult.php
+++ src/ValueObject/PdfDebugComparisonResult.php
@@ -6,13 +6,9 @@ final class PdfDebugComparisonResult
 {
     public function __construct(
         public readonly int $pageNumber,
-        public readonly string $actualPath,
-        public readonly ?string $referencePath,
-        public readonly ?string $diffPath,
         public readonly ?float $distortion,
         public readonly bool $missingReference,
-    ) {
-    }
+    ) {}
 
     public function hasPassed(float $threshold = 0.001): bool
     {
